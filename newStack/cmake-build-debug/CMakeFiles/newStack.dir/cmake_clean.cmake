377125d762024c51e8858924e409de52d1095924
Arv1k <you@example.com>
2019-02-27 00:48:41 +0300

    C++ stack on classes.

---
+++ newStack/cmake-build-debug/CMakeFiles/newStack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newStack.dir/main.cpp.o"
+  "newStack.pdb"
+  "newStack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/newStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
